f6b28f55a127cfef25497194cc18a0a3d16312c0
Vlad <fromvladvasylyshyn@gmail.com>
2024-06-10 19:14:45 +0300

    and

--- back/routes/router.js
+++ back/routes/router.js
@@ -13,7 +13,7 @@ router.use('/order', orderRouter);
 
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../front', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../front', 'index.html'));
 });
 router.use('/', productRouter);
 export default router;
